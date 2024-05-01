f97af09968af9a633d3440c7036ab23c8dbc8a52
anykey <billykidd@mail.ru>
2024-05-01 20:32:11 +0300

    review_1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/chelobacca/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function addCard(element) {
+const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
+
+cardElement.querySelector('.card__image').src = element.link;
+cardElement.querySelector('.card__title').textContent = element.name;
+
+const deleteButton = cardElement.querySelector('.card__delete-button');
+deleteButton.addEventListener('click', function () {
+    const listItem = deleteButton.closest('.places__item');
+    listItem.remove();
+  }); 
+return cardElement;
+};
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (item) {
+placesList.append(addCard(item));
+});
+
+
+
+
+
+  
+
+  
+
+
+
+
