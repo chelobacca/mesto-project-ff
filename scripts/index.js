c5582ef458674568a6a998b4387f0e6415728a26
anykey <billykidd@mail.ru>
2024-05-03 23:02:03 +0300

    review_3.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,35 @@
-const cardTemplate = document.querySelector('#card-template').content;
-const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
 // СОЗДАНИЕ КАРТОЧКИ
 function createCard(cardData, removeHandler) {
-const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// наполнение карточки
-const imgSelector = cardElement.querySelector('.card__image');
-imgSelector.src = cardData.link;
-imgSelector.alt = cardData.name;
-cardElement.querySelector('.card__title').textContent = cardData.name;
+  // наполнение карточки
+  const imgSelector = cardElement.querySelector(".card__image");
+  imgSelector.src = cardData.link;
+  imgSelector.alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
+  // удаление карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const currentCard = deleteButton.closest(".places__item");
+  deleteButton.addEventListener("click", function () {
+    removeHandler(currentCard);
+  });
 
-// удаление карточки
-const deleteButton = cardElement.querySelector('.card__delete-button');
-const currentCard = deleteButton.closest('.places__item');
-deleteButton.addEventListener('click', function() {
-  removeHandler(currentCard)
-}); 
-
-// возврат элемента карточки
-return cardElement;
-};
+  // возврат элемента карточки
+  return cardElement;
+}
 
 // ВЫВОД КАРТОЧЕК
 initialCards.forEach(function (item) {
-placesList.append(createCard(item, deleteCard));
+  placesList.append(createCard(item, deleteCard));
 });
 
 // ХЭНДЛЕР УДАЛЕНИЯ
 function deleteCard(card) {
-card.remove();
-};
+  card.remove();
+}
